bb33989d01d7e32f0b749f771f805ed83826f599
gnwncpta <gnwncpta@gmail.com>
2021-12-20 00:28:09 +0700

    add: artworks

---
+++ pages/artworks.jsx
@@ -0,0 +1,7 @@
+export default function Artworks(){
+    return (
+        <section className="bg-black w-full h-screen flex items-center justify-center">
+            <h1 className="py-3 px-8 text-white text-2xl rounded-full border border-white">On Working</h1>
+        </section>
+    )
+}
