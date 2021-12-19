84f64e166b522e6bbcc42633f469f5fcf63477d3
gnwncpta <gnwncpta@gmail.com>
2021-12-20 00:28:17 +0700

    update: index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,6 +3,8 @@ import { useEffect } from 'react';
 
 // Components
 import Part from '../components/part';
+import ButtonActive from '../components/buttons/button-active';
+import ButtonBlack from '../components/buttons/button-black';
 
 export default function Home(props){
 
@@ -21,14 +23,14 @@ export default function Home(props){
                     <div className="left-side w-max mt-32">
                         <p className="text-white font-normal">Welcome to Portfolio</p>
                         <h1 className="mt-3 text-white font-semibold text-6xl leading-tight">Explore Inspired <br/> Projects.</h1>
-                        <p className="text-white text-sm mt-16 leading-normal">Welcome to my portfolio site, where you can find any project that<br/>
+                        <p className="text-white text-sm mt-16 font-light leading-relaxed">Welcome to my portfolio site, where you can find any project that<br/>
                             i have done. So feel free to explore my projects in this website</p>
                         
-                        <button className="py-2 px-8 mt-10 text-sm bg-lime-400 text-black rounded-full">Explore</button>
+                        <ButtonActive name="Explore" />
 
                         <div className="social-media-container mt-32">
-                            <button className="text-white text-sm">--- Instagram</button>
-                            <button className="text-white text-sm ml-40">--- Portfolio</button>
+                            <button className="text-white text-sm py-2 px-8 border border-transparent rounded-full hover:border-white">Instagram</button>
+                            <button className="text-white text-sm ml-24 py-2 px-8 border border-transparent rounded-full hover:border-white">Portfolio</button>
                         </div>
                     </div>
 
@@ -37,14 +39,14 @@ export default function Home(props){
                             <div className="gambar left-0 top-0 w-full h-96 bg-white"></div>
 
                             <div className="px-8 py-6 mt-10 ml-6">
-                                <h1 className="font-semibold text-white text-3xl">Introduce.</h1>
+                                <h1 className="font-semibold text-white text-4xl">Introduce.</h1>
 
-                                <p className="text-white text-sm my-6">Hi! I'm Gunawan Cipta, 18 Year Old.</p>
+                                <p className="text-white text-sm font-light my-6">Hi! I'm Gunawan Cipta, 18 Year Old.</p>
 
-                                <p className="text-white text-sm">I am a Graphic Designer who love to <br />
+                                <p className="text-white text-sm font-light">I am a Graphic Designer who love to <br />
                                     design a thing. And also i am a Programmer.</p>
 
-                                <button className="text-white text-sm py-2 px-6 border border-white rounded-full mt-10 hover:bg-lime-400 hover:text-black hover:border-lime-400">Download My CV</button>
+                                <button className="text-white text-sm py-2 px-6 border border-white rounded-full mt-10 hover:bg-lime-400 hover:text-black hover:border-lime-400 hover:italic">Download My CV</button>
                             </div>
                         </div>
                     </div>
@@ -57,13 +59,14 @@ export default function Home(props){
                 <img src="/graphic-design-sec.png" alt="graphic-design-image" className="w-2/4" />
 
                 <div className="bg-lime-400 w-2/4 flex items-center justify-center">
-                    <div className="w-2/4 text-right">
+                    <div className="w-2/4 flex flex-col items-end text-right">
+                        <p className="w-max py-1 px-2 text-white text-xs rounded-full bg-black">#GraphicDesign</p>
                         <h1 className="my-6 font-semibold text-6xl leading-tight">Graphic Design<br/>Projects.</h1>
 
-                        <p className="text-sm leading-normal">Welcome to my portfolio site, where you can find any project that <br/>
+                        <p className="text-sm mt-12 leading-normal">Welcome to my portfolio site, where you can find any project that <br/>
                             i have done. So feel free to explore my projects in this website</p>
 
-                        <button className="py-2 px-8 mt-10 text-sm rounded-full bg-black text-white">Explore</button>
+                        <ButtonBlack name="Explore" />
                     </div>
                 </div>
             </div>
@@ -73,16 +76,27 @@ export default function Home(props){
             <div className="h-1/4 flex">
                 <div className="bg-lime-400 w-2/4 flex items-center justify-center">
                     <div className="w-2/4">
+                        <p className="w-max py-1 px-2 text-white text-xs rounded-full bg-black">#Programming</p>
                         <h1 className="my-6 font-semibold text-6xl leading-tight">Programming<br/>Projects.</h1>
 
-                        <p className="text-sm leading-normal">Welcome to my portfolio site, where you can find any project that <br/>
+                        <p className="text-sm mt-12 leading-normal">Welcome to my portfolio site, where you can find any project that <br/>
                             i have done. So feel free to explore my projects in this website</p>
 
-                        <button className="py-2 px-8 mt-10 text-sm rounded-full bg-black text-white">Explore</button>
+                        <ButtonBlack name="Explore" />
                     </div>
                 </div>
 
                 <img src="/programming-image.png" alt="graphic-design-image" className="w-2/4" />
+            </div>
+
+            <div className="py-96 flex items-center justify-center">
+                <div className="text-center">
+                    <h1 className="text-white font-semibold text-6xl">Interested ?</h1>
+                    <p className="text-white text-sm mt-10 leading-relaxed font-light">If you want to work with me you can <br/>
+                        Email me or DM me via Instagram.</p>
+
+                    <ButtonActive name="Email" />
+                </div>
             </div>
 
         </section>
